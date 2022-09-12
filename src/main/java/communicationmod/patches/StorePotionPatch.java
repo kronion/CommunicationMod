94cc9cabfe556f24236ebfdb3f993fd70226b012
Zeus Kronion <kronion@users.noreply.github.com>
2022-09-12 00:39:17 -0500

    Use the correct method for registering an update

--- src/main/java/communicationmod/patches/StorePotionPatch.java
+++ src/main/java/communicationmod/patches/StorePotionPatch.java
@@ -2,7 +2,7 @@ package communicationmod.patches;
 
 import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.shop.StorePotion;
-import communicationmod.CommunicationMod;
+import communicationmod.GameStateListener;
 
 
 public class StorePotionPatch {
@@ -15,7 +15,7 @@ public class StorePotionPatch {
         public static void Postfix() {
             // Without this there's no state change if potion slots are full,
             // or if the player has the Sozu relic.
-            CommunicationMod.mustSendGameState = true;
+            GameStateListener.registerStateChange();
         }
 
     }

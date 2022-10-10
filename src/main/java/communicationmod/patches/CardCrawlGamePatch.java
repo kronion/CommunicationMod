43e7bf23f55c1e99e2759dd8af0656fc8204904f
Zeus Kronion <kronion@users.noreply.github.com>
2022-10-10 17:42:13 -0500

    Base rendering off the value of a static boolean

--- src/main/java/communicationmod/patches/CardCrawlGamePatch.java
+++ src/main/java/communicationmod/patches/CardCrawlGamePatch.java
@@ -2,7 +2,7 @@ package communicationmod.patches;
 
 import com.badlogic.gdx.Gdx;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.ExceptionHandler;
 import com.megacrit.cardcrawl.daily.TimeHelper;
@@ -13,22 +13,32 @@ import org.apache.logging.log4j.Logger;
 public class CardCrawlGamePatch {
     private static final Logger logger = LogManager.getLogger(CardCrawlGamePatch.class.getName());
 
+    static class RenderState {
+        static boolean render = false;
+    }
+
     @SpirePatch(
             clz = CardCrawlGame.class,
             method = "render"
     )
     public static class RenderPatch {
-        public static void Replace(CardCrawlGame __instance) {
-            try {
-                TimeHelper.update();
-                if (Gdx.graphics.getRawDeltaTime() > 0.1F)
-                    return;
-                __instance.update();
-            } catch (Exception e) {
-                logger.info("Exception occurred in CardCrawlGame render method!");
-                ExceptionHandler.handleException(e, logger);
-                Gdx.app.exit();
+        public static SpireReturn<Void> Prefix(CardCrawlGame __instance) {
+            if (!RenderState.render) {
+                try {
+                    TimeHelper.update();
+                    if (Gdx.graphics.getRawDeltaTime() > 0.1F)
+                        return SpireReturn.Return();
+                    __instance.update();
+                } catch (Exception e) {
+                    logger.info("Exception occurred in CardCrawlGame render method!");
+                    ExceptionHandler.handleException(e, logger);
+                    Gdx.app.exit();
+                }
+
+                return SpireReturn.Return();
             }
+
+            return SpireReturn.Continue();
         }
     }
 
